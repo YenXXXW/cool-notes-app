4fbf8c91fd361b7a33558688ac7fd3711d956a86
Yen <YenXXXW@gmail.com>
2023-09-10 22:41:07 +0630

    added the localhost origin

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://cool-notes-app.vercel.app",
+    origin: ["https://cool-notes-app.vercel.app", "http://localhost:5173"],
     credentials: true,
   })
 );
